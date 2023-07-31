99133bd2068ba669301272459f567aafdc934247
Marcelo <marcelorcarnauba@gmail.com>
2023-07-31 08:38:43 -0300

    test-commit

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ const imagem = document.querySelectorAll('.product-div img')
 categorias.forEach((categoria) => {
   categoria.addEventListener('click', () => {
     const nomeCategoria = categoria.querySelector('p')
-    window.location.href = 'Catetories-Page.html'
+    window.location.href = 'html/Catetories-Page.html'
     localStorage.setItem('tituloCategoria', `${nomeCategoria.innerHTML} - LOGO`)
   })
 })
 
 produtos.forEach((produto, index) => {
   produto.addEventListener('click', () => {
-    window.location.href = 'Product-Page.html'
+    window.location.href = 'html/Product-Page.html'
     localStorage.setItem('tituloProduto', `${produto.innerHTML} - LOGO`)
     localStorage.setItem('nomeProduto', `${produto.innerHTML}`)
     localStorage.setItem('precoProduto', preco[index].innerHTML)

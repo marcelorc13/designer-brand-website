35956a62e197c181917c2baa84b0376c42b2fe53
Marcelo <marcelorcarnauba@gmail.com>
2023-08-03 16:25:00 -0300

    test-commit-02

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [""],
+  theme: {
+    extend: {
+      colors: {
+        prncipal: '#c2b792',
+        secundaria: '#706850',
+      },
+    },
+  },
+  plugins: [],
+}
+

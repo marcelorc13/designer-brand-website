3f34c14f308eb618fd5ba0941c6ece5d377a639a
Marcelo <marcelorcarnauba@gmail.com>
2023-08-03 20:08:42 -0300

    product-page-commit-04

--- js/productPage.js
+++ js/productPage.js
@@ -11,13 +11,24 @@ const botaoCarrinho = document.querySelector('.shopping-bag')
 
 const iconeCarrinho = document.querySelector('#numero-carrinho')
 
+const adicionadoAoCarrinho = document.querySelector('#adicionado-ao-carrinho')
+
 nomeProduto.innerHTML = nome
 precoProduto.innerHTML = preco
 imagemProduto.src = imagem
 
 document.title = titulo
 
-botaoCarrinho.addEventListener("click", () => {
+botaoCarrinho.addEventListener("click", () => adicionarAoCarrinho())
+
+function adicionarAoCarrinho() {
     iconeCarrinho.innerHTML ++
-})
+    adicionadoAoCarrinho.classList.remove('hidden') 
+    adicionadoAoCarrinho.classList.add('flex') 
+    setTimeout (esconderAdicionado, 3000)
+}
+
+function esconderAdicionado() {
+    adicionadoAoCarrinho.classList.add('hidden')
+}
 
